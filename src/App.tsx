eb9d59edc830239be44c43a6e05e41567debb607
vboyarkina <vboyarkina@zuzex.com>
2024-07-12 15:52:24 +0300

    feat:add data selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
 import { useState } from 'react'
 import SearchPlace from './components/SearchPlace'
+import SideBar from './components/SideBar'
 import { Place } from './types'
 
 function App() {
   const [currentPlace, setCurrentPlace] = useState<Place | null>(null)
-
   return (
-    <div className="flex flex-col min-h-screen font-Roboto bg-weather-primary">
-      <main className="container text-white">
+    <div className="grid grid-rows-3 grid-flow-col gap-4 min-h-screen font-Roboto bg-weather-primary">
+      <SideBar />
+      <header className="col-span-2">
         <div className="flex flex-col gap-4">
           <SearchPlace onSetCurrentPlace={setCurrentPlace} />
         </div>
-      </main>
+      </header>
+
+      <main className="row-span-2 col-span-2"></main>
+      {/* 
       {currentPlace && (
         <div className=" text-white py-2 bg-weather-secondary w-full text-center">
           <div className="container">
             <p>Location: {currentPlace.placeName}</p>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   )
 }
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,9 @@
+import DateRangePicker from './DateRangePicker'
+
+export default function SideBar() {
+  return (
+    <aside className="row-span-3">
+      <DateRangePicker />
+    </aside>
+  )
+}
